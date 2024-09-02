a6f5c74dbe26e3eada2b520f3b2c1c79ab0c126f
milenaxntj <michurata@gmail.com>
2024-09-01 23:59:11 -0300

    Iniciando tela de Criar Carteirinha

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
-import { Route, RouterProvider } from "react-router-dom"
-import { createBrowserRouter } from "react-router-dom"
-import Home from './pages/Home'
-import MainLayout from './layouts/MainLayout'
-import Login from './pages/Login'
-import Cadastro from './pages/Cadastro'
+import React from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+import MainLayout from './layouts/MainLayout';
+import Login from './pages/Login';
+import Cadastro from './pages/Cadastro';
+import CriarCarteirinha from './pages/CriarCarteirinha';
+import VerCarteirinha from './pages/VerCarteirinha';
+import Creditos from './pages/Creditos';
 
 const router = createBrowserRouter([
-  { path: "/", element: <MainLayout />, children: [
-    { path: "/", element: <Home /> },
-    { path: "/login", element: <Login />},
-    { path: "/cadastro", element: <Cadastro />}
-  ] }],
-)
+  {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/login', element: <Login /> },
+      { path: '/cadastro', element: <Cadastro /> },
+      { path: '/criarcarteirinha', element: <CriarCarteirinha /> },
+      { path: '/vercarteirinha', element: <VerCarteirinha /> },
+      { path: '/creditos', element: <Creditos /> },
+    ],
+  },
+]);
 
 const App = () => {
-  return <RouterProvider router={router} />
-}
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
