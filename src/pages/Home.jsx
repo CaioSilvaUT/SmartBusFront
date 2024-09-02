2df534f28385e54a19a01f72842cf1ba696b115e
milenaxntj <michurata@gmail.com>
2024-09-01 22:00:52 -0300

    Criando tela Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,38 @@
+import "./Home.css";
+import olho_icon from "../assets/olho-icon.png";
+import historico_icon from "../assets/historico-icon.png";
+
 const Home = () => {
   return (
-    <> <p>home</p>
-    </>
-  )
-}
+    <div className="home">
+      <section className="infos">
+        <div className="saldo">
+          <p>Saldo: R$</p>
+          {/* Conectar com o backend para exibir o saldo real */}
+          <p className="amount">0,00</p>
+        </div>
+        <div className="buttons">
+          <button>Visualizar cartão</button>
+        </div>
+      </section>
+      <section className="functions">
+        <div className="box">
+          <div className="visualizar_horarios">
+            <div className="icon-placeholder">
+              <img src={olho_icon} alt="Ícone para Visualizar Horários" />
+            </div>
+            <p>Visualizar horários</p>
+          </div>
+          <div className="visualizar_historico">
+            <div className="icon-placeholder">
+              <img src={historico_icon} alt="Ícone para Visualizar Histórico" />
+            </div>
+            <p>Visualizar histórico</p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
