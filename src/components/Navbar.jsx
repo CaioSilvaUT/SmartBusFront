9f58a18460a41291fd311e66408a89707740ec53
Ingrid <ingridreupke@alunos.utfpr.edu.br>
2024-09-03 15:50:53 -0300

    Auth e tela de perfil do usuário

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,101 @@
-import {Link} from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { useLogoutMutation } from '../auth/usersApiSlice';
+import { logout } from '../auth/authSlice';
+import { useState } from 'react';
+import { useEffect } from 'react'
+import axios from 'axios';
 
 const Navbar = () => { 
+  const { userInfo } = useSelector((state => state.auth));
+  const [resData, setResData] = useState("")
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [dropdown, setDropdown] = useState(false);
+
+  const handleMessage = () => {
+    axios.get(`http://localhost:3000/showUserId/${userInfo.data.id}`, {
+      params: {
+        id: userInfo.data.id
+      }
+    })
+    .then(res => {
+      console.log(res)
+      setResData(res.data)
+    })
+    .catch(err => console.log(err))
+  }
+
+  const logoutHandle = async () => {
+    try {
+      await axios.post("http://localhost:3000/logout");
+      dispatch(logout());
+      navigate('/home');
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleDropdown = () => {
+    setDropdown(!dropdown);
+    handleMessage()
+  };
+
   return (
     <>
-    <nav className="bg-green-200 p-4">
+      <nav className="bg-green-200 p-4">
         <div className="font-inter container mx-auto flex justify-between items-center">
-            <Link to="/" className="text-white text-xl font-bold">SmartBus</Link>
-            <div className="space-x-4">
-                <Link to="/" className="text-white font-semibold hover:text-green-300">Home</Link>
-                <Link to="/login" className="text-white font-semibold hover:text-green-300">Login</Link>
-            </div>
+          <div className="flex items-center">
+            {userInfo ? (
+              <NavLink to="/" className="text-white text-xl font-bold">SmartBus</NavLink>
+            ) : (
+              <NavLink to="/home" className="text-white text-xl font-bold">SmartBus</NavLink>
+            )}
+          </div>
+          <div className="space-x-4">
+            {userInfo ? (
+              <>
+                <NavLink to="/" className={({isActive }) => isActive ? 'text-white font-semibold bg-green-400 rounded p-2 shadow-md' : 'text-white font-semibold hover:text-green-300 p-2'}>Home</NavLink>
+                <NavLink to="#" className={({isActive }) => isActive ? 'text-white font-semibold bg-green-400 rounded p-2 shadow-md' : 'text-white font-semibold hover:text-green-300 p-2'}>Linhas</NavLink>
+
+                <div className="relative inline-block">
+                  <button
+                    onMouseEnter={handleDropdown}
+                    className="text-white font-semibold hover:text-green-300 focus:outline-none px-2">
+                    Conta
+                    <svg className="inline-block w-4 h-4 ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path>
+                    </svg>
+                  </button>
+                 
+                  {dropdown && (
+                    <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-10">
+                      <div className="border-b-2 border-green-300 px-4 py-3 text-black">
+                      <div>Olá, {resData.nome}</div>
+                      <div className="font-medium truncate">{resData.email}</div>
+                    </div>
+                      <NavLink to="/perfil" className="block px-4 py-2 text-black hover:bg-gray-200">Perfil</NavLink>
+                      <NavLink to="#" className="block px-4 py-2 text-black hover:bg-gray-200">Cartão</NavLink>
+                      <NavLink to="#" className="block px-4 py-2 text-black hover:bg-gray-200">Histórico</NavLink>
+                      <button
+                        onClick={logoutHandle}
+                        className="block w-full text-left px-4 py-2 text-black hover:bg-gray-200">Sair
+                      </button>
+                    </div>
+                  )}
+                </div>
+              </>
+            ) : (
+              <>
+                <NavLink to="/home" className={({isActive }) => isActive ? 'text-white font-semibold bg-green-400 rounded p-2 shadow-md' : 'text-white font-semibold hover:text-green-300 p-2'}>Home</NavLink>
+                <NavLink to="/login" className={({isActive }) => isActive ? 'text-white font-semibold bg-green-400 rounded p-2 shadow-md' : 'text-white font-semibold hover:text-green-300 p-2'}>Login</NavLink>
+              </>
+            )}
+          </div>
         </div>
-    </nav>
+      </nav>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -1,41 +1,51 @@
 import {Link} from 'react-router-dom'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 const Cadastro = () => {
     const navigate = useNavigate()
-    const [values, setValues] = useState({
-        name: '',
-        email: '',
-        password: ''
-    })
-
-    const handleInput = (e) => {
-        setValues(p => ({
-            ...p, [e.target.name]: [e.target.value]
-        }))
-    }
     
-    const handleSubmit = (e) => {
+    const [zoomIn, setZoomIn] = useState('scale-95 opacity-0')
+
+    const [nome, setNome] = useState("")
+    const [email, setEmail] = useState("")
+    const [telefone, setTelefone] = useState("")
+    const [senha, setSenha] = useState("")
+
+    const handleSubmit = async (e) => {
         e.preventDefault()
-        navigate('/login')
+        await axios.post("http://localhost:3000/newUser", {
+            nome: nome,
+            email: email,
+            telefone: telefone,
+            senha: senha,
+        }).then((res) => {
+            navigate('/login')
+        })
     }
+
+    useEffect( () => {
+        setZoomIn('scale-100 opacity-100')
+    })
 
   return (
     <>
-    <div className="shadow-lg bg-beige-100 h-screen flex items-center justify-center">
-    <div className="shadow-lg bg-green-200 p-8 w-full max-w-3xl flex flex-col items-center">
+    <div className="bg-green-200">
+    <div className="shadow-lg bg-green-300 h-screen flex items-center justify-center border-8 border-green-200 rounded-3xl">
+    <div className={`shadow-lg bg-green-200 p-8 rounded w-full max-w-3xl flex flex-col items-center transition-transform transition-opacity duration-1000 ease-out ${zoomIn}`}>
         <div className="bg-green-100 bg-opacity-60 p-8 w-full flex flex-col items-center">
-            <h1 className="text-white text-3xl font-inter font-extrabold mb-6">Cadastro</h1>
+            <h1 className="text-white text-3xl font-inter font-extrabold mb-6 drop-shadow-sm">Cadastro</h1>
             <form onSubmit={handleSubmit} className="space-y-4 w-2/3 flex-col items-center justify-center">
                 <div>
-                    <label htmlFor="name" className="font-bold font-inter block text-white">Nome</label>
+                    <label htmlFor="nome" className="font-bold font-inter block text-white">Nome</label>
                     <input type="text"
-                     id="name" 
-                     name="name"
+                     id="nome" 
+                     name="nome"
                      className="w-full py-1 px-4 border-2 border-grey rounded-lg bg-transparent text-white focus:outline-none focus:ring-2 focus:ring-green-200" 
                      required
-                     onChange={handleInput}></input>
+                     onChange={(e) => {setNome(e.target.value)}}></input>
                 </div>
                 <div>
                     <label htmlFor="email" className="font-bold font-inter block text-white">E-mail</label>
@@ -44,25 +54,26 @@ const Cadastro = () => {
                     name="email" 
                     className="w-full py-1 px-4 border-2 border-grey rounded-lg bg-transparent text-white focus:outline-none focus:ring-2 focus:ring-green-200" 
                     required
-                    onChange={handleInput}></input>
+                    onChange={(e) => {setEmail(e.target.value)}}></input>
                 </div>
                 <div>
-                    <label htmlFor="password" className="font-bold font-inter block text-white">Senha</label>
-                    <input type="password" 
-                    id="password" 
-                    name="password" 
-                    className="w-full py-1 px-4 border-2 border-grey rounded-lg bg-transparent text-white focus:outline-none focus:ring-2 focus:ring-green-200" 
+                    <label htmlFor="telefone" className="font-bold font-inter block text-white">Telefone</label>
+                    <input type="tel" 
+                    id="telefone" 
+                    name="telefone"
+                    className="w-full py-1 px-4 border-2 border-grey rounded-lg bg-transparent text-white focus:outline-none focus:ring-2 focus:ring-green-200"
                     required
-                    onChange={handleInput}></input>
+                    onChange={(e) => {setTelefone(e.target.value)}}></input>
                 </div>
                 <div>
-                    <label htmlFor="confirm-password" className="font-bold font-inter block text-white">Confirmar senha</label>
+                    <label htmlFor="senha" className="font-bold font-inter block text-white">Senha</label>
                     <input type="password" 
-                    id="confirm-password" name="confirm-password" 
+                    id="senha" 
+                    name="senha" 
                     className="w-full py-1 px-4 border-2 border-grey rounded-lg bg-transparent text-white focus:outline-none focus:ring-2 focus:ring-green-200" 
                     required
-                    onChange={handleInput}></input>
-                </div>
+                    onChange={(e) => {setSenha(e.target.value)}}></input>
+                    </div>
                 <div className="mt-4 flex justify-center">
                     <button type="submit" className="bg-transparent font-bold font-inter text-white py-1 px-4 border-2 border-grey rounded-lg hover:bg-green-400 transition duration-300">Confirmar</button>
                 </div>
@@ -72,6 +83,7 @@ const Cadastro = () => {
             </div>
         </div>
     </div>
+</div>
 </div>
     </>
   )
